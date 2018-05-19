c84d7212d69efe2a6ff6f06c24bbcf5988895e33
Renée Kooi <renee@kooi.me>
2018-05-19 13:32:28 +0200

    Fix status URL behind reverse proxy.

--- src/manage.js
+++ src/manage.js
@@ -3,14 +3,14 @@ const status = document.querySelector('#status')
 const toggle = document.querySelector('#toggle')
 
 function render () {
-  fetch('/status').then((res) => res.json()).then((body) => {
+  fetch('./status').then((res) => res.json()).then((body) => {
     status.textContent = body.data.value ? 'Enabled' : 'Disabled'
     toggle.textContent = body.data.value ? 'Disable' : 'Enable'
   })
 }
 
 toggle.onclick = () => {
-  fetch('/status', {
+  fetch('./status', {
     method: 'post'
   }).then(render)
 }
